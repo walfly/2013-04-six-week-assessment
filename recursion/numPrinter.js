c8a66a45c49d66ca781f94593cb09b722c445783
Walker Flynn <walker.d.flynn@gmail.com>
2013-06-17 11:14:40 -0700

    recursion done

--- recursion/numPrinter.js
+++ recursion/numPrinter.js
@@ -1,2 +1,8 @@
 /* CODE GOES HERE
  * Don't forget newlines at the end of all files :) */
+var reccursPrint = function(array){
+  console.log(array.shift());
+  if(array.length){
+    reccursPrint(array);
+  }
+}
