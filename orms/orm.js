ce65dae011bea841c63ac3e0e8b4a0bc6878e7e4
Walker Flynn <walker.d.flynn@gmail.com>
2013-06-17 12:23:19 -0700

    finished up to ORMs just back bone left

--- orms/orm.js
+++ orms/orm.js
@@ -11,5 +11,10 @@ var User = function(name, email) {
 User.prototype.constructor = User;
 
 User.prototype.save = function() {
-  // ========YOUR CODE HERE===========
+  var check = query("SELECT name FROM users WHERE name =" + this.name);
+  if(!check){
+    query("NSERT INTO users (name, email) VALUES ("+this.name+","+this.email+")")
+  } else {
+  	query("UPDATE users SET name ="+this.name+", email="+this.email+" WHERE name = "this.name";");
+  }
 };
