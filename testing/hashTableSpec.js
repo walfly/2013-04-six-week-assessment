33ce9c2a633daf505ad97f2c963e20a08bc6c3c5
Walker Flynn <walker.d.flynn@gmail.com>
2013-06-17 11:08:40 -0700

    testing done

--- testing/hashTableSpec.js
+++ testing/hashTableSpec.js
@@ -1,1 +1,14 @@
 // Create your tests for the hashTable here...
+describe("#hashTable", function(){
+  var hasht = makeHash();
+  beforeEach(function(){
+    hasht.insert('poop', 5);
+    hasht.insert('seven', 'ten');
+    hasht.insert('five', 5);
+  });
+  it('should return the value when searched by key', function(){
+    var ret = hasht.retrieve('seven');
+    //the hashtable is wrong
+    expect(ret.key).toEqual('five');
+  });
+});
