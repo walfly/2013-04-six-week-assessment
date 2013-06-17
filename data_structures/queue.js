33bddfcb91a2fedb1626ea4c34d0ff6ac4fe6377
Walker Flynn <walker.d.flynn@gmail.com>
2013-06-17 10:36:31 -0700

    data structures done

--- data_structures/queue.js
+++ data_structures/queue.js
@@ -1,2 +1,11 @@
 /* CODE GOES HERE
  * Don't forget newlines at the end of all files :) */
+var Queue = function() {
+	var storage = [];
+	this.add = function(item){
+		storage.push(item);
+	}
+  this.remove = function(){
+    return storage.shift(item);
+  }
+};
